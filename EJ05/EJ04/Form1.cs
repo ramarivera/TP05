afc7607df7c374685734fb0810633748b2d34392
martinnarrua <martin94.profugo@gmail.com>
2015-11-13 15:01:21 -0300

    ej4 listo

---
+++ EJ05/EJ04/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EJ04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text += this.button1.Text + "\r\n";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text += this.button2.Text + "\r\n";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text += this.button3.Text + "\r\n";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text += this.button4.Text + "\r\n";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text += this.button5.Text + "\r\n";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text += this.button6.Text + "\r\n";
+        }
+    }
+}
