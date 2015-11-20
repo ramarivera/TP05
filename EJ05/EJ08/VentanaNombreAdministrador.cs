cc6e48a4f3ebd6cbca1a323f918c2c96665cc5e1
martinnarrua <martin94.profugo@gmail.com>
2015-11-20 18:48:29 -0300

    comentarios y ajustes en VentanaNombreAdministrador, falta fachada

--- EJ05/EJ08/VentanaNombreAdministrador.cs
+++ EJ05/EJ08/VentanaNombreAdministrador.cs
@@ -10,17 +10,32 @@ using System.Windows.Forms;
 
 namespace EJ08
 {
+    /// <summary>
+    /// Representa la lógica de la <see cref="VentanaNombreAdministrador"/>
+    /// </summary>
     public partial class VentanaNombreAdministrador : Form
     {
+        /// <summary>
+        /// Propiedad Nombre
+        /// </summary>
         public string Nombre
         {
             get { return this.txtNombre.Text; }
         }
+
+        /// <summary>
+        /// Inicializa una nueva instancia de <see cref="VentanaNombreAdministrador"/>
+        /// </summary>
         public VentanaNombreAdministrador()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Representa lo que ocurre al presionar el boton Acepar
+        /// </summary>
+        /// <param name="sender">La fuente del evento.</param>
+        /// <param name="e">La instancia de  <see cref="EventArgs"/> que contiene informacion del evento.</param>
         private void btnAceptar_Click(object sender, EventArgs e)
         {
             this.Close();
