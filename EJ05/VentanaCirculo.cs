10ddf2724b695b4b5ef235b36c81acffbfbb2b57
RamaRivera <ramarivera@gmail.com>
2015-11-20 19:50:36 -0300

    Cambios en la estructura de las carpetas

--- EJ05/VentanaCirculo.cs
+++ EJ05/VentanaCirculo.cs
@@ -10,51 +10,70 @@ using System.Windows.Forms;
 
 namespace EJ05
 {
+    /// <summary>
+    /// Representa la lógica de la ventana principal de la aplicación
+    /// </summary>
     public partial class VentanaCirculo : Form
     {
         Facade cFachada = new Facade();
+
+        /// <summary>
+        /// Constructor de la clase
+        /// </summary>
         public VentanaCirculo()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Representa lo que ocurre al presionar el botón especificado. Cierra la ventana actual
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button3_Click(object sender, EventArgs e)
         {
             this.Close();
-            /*Form1 menu = new Form1();
-            menu.Show();*/ //Ver el boton siguiente de la ventana anterior
         }
 
+        /// <summary>
+        /// Representa lo que ocurre al presionar el botón especificado. Muestra el perímetro del círculo
+        /// con los datos ingresados
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
-            if ((this.textBox1.Text == "") || (this.textBox2.Text=="") || (this.textBox3.Text == ""))
+            double coorX, coorY, radio;
+            if ( !(double.TryParse(this.txtCoordX.Text,out coorX)) || !(double.TryParse(this.txtCoordY.Text, out coorY)) || !(double.TryParse(this.txtRadio.Text, out radio)))
             {
-                MessageBox.Show("Faltan datos");
+                MessageBox.Show("Faltan datos o los ingresados no son números","Verifique los datos");
             }
             else
             {
-                double per =this.cFachada.CalcularPerimetroCirculo(double.Parse(this.textBox1.Text),double.Parse(this.textBox2.Text),double.Parse(this.textBox3.Text));
+                double per =this.cFachada.CalcularPerimetroCirculo(coorX,coorY,radio);
                 MessageBox.Show("El perímetro del círculo es: "+ per.ToString(),"Perímetro");
             }
 
         }
 
+        /// <summary>
+        /// Representa lo que ocurre al presionar el botón especificado. Muestra el área del círculo
+        /// con los datos ingresados
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button2_Click(object sender, EventArgs e)
         {
-            if ((this.textBox1.Text == "") || (this.textBox2.Text == "") || (this.textBox3.Text == ""))
+            double coorX, coorY, radio;
+            if (!(double.TryParse(this.txtCoordX.Text, out coorX)) || !(double.TryParse(this.txtCoordY.Text, out coorY)) || !(double.TryParse(this.txtRadio.Text, out radio)))
             {
-                MessageBox.Show("Faltan datos");
+                MessageBox.Show("Faltan datos o los ingresados no son números", "Verifique los datos");
             }
             else
             {
-                double area = this.cFachada.CalcularAreaCirculo(double.Parse(this.textBox1.Text), double.Parse(this.textBox2.Text), double.Parse(this.textBox3.Text));
+                double area = this.cFachada.CalcularAreaCirculo(coorX,coorY,radio);
                 MessageBox.Show("El área del círculo es: " + area.ToString(), "Área");
             }
-        }
-
-        private void VentanaCirculo_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
--- EJ05/VentanaTriangulo.cs
+++ EJ05/VentanaTriangulo.cs
@@ -10,46 +10,75 @@ using System.Windows.Forms;
 
 namespace EJ05
 {
+    /// <summary>
+    /// Representa la lógica de la ventana principal de la aplicación
+    /// </summary>
     public partial class VentanaTriangulo : Form
     {
         Facade cFachada = new Facade();
+
+        /// <summary>
+        /// Constructor de la clase
+        /// </summary>
         public VentanaTriangulo()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Representa lo que ocurre al presionar el botón especificado. Cierra la ventana actual
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button3_Click(object sender, EventArgs e)
         {
             this.Close();
-            /*Form1 menu = new Form1();
-            menu.Show();*/ //Ver el boton siguiente de la ventana anterior
         }
 
+        /// <summary>
+        /// Representa lo que ocurre al presionar el botón especificado. Muestra el perímetro del triángulo
+        /// con los datos ingresados
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
-            if ((this.textBox1.Text == "") || (this.textBox2.Text == "") || (this.textBox3.Text == "") || (this.textBox4.Text == "") || (this.textBox5.Text == "") || (this.textBox6.Text == ""))
+            double p1coorX, p1coorY, p2coorX, p2coorY, p3coorX, p3coorY;
+            if (!(double.TryParse(this.txtP1CoordX.Text, out p1coorX)) ||
+                !(double.TryParse(this.txtP1CoordY.Text, out p1coorY)) ||
+                !(double.TryParse(this.txtP2CoordX.Text, out p2coorX)) ||
+                !(double.TryParse(this.txtP2CoordY.Text, out p2coorY)) ||
+                !(double.TryParse(this.txtP3CoordX.Text, out p3coorX)) ||
+                !(double.TryParse(this.txtP3CoordY.Text, out p3coorY)))
             {
-                MessageBox.Show("Faltan datos");
+                MessageBox.Show("Faltan datos o los ingresados no son números", "Verifique los datos");
             }
             else
             {
-                double[] coorX = new double[] { double.Parse(this.textBox1.Text), double.Parse(this.textBox3.Text), double.Parse(this.textBox5.Text) };
-                double[] coorY = new double[] { double.Parse(this.textBox2.Text), double.Parse(this.textBox4.Text), double.Parse(this.textBox6.Text) };
+                double[] coorX = new double[] { p1coorX,p2coorX,p3coorX};
+                double[] coorY = new double[] { p1coorY,p2coorY,p3coorY};
                 double per = this.cFachada.CalcularPerimetroTriangulo(coorX,coorY);
                 MessageBox.Show("El perímetro del triángulo es: " + per.ToString(), "Perímetro");
             }
         }
 
+        /// <summary>
+        /// Representa lo que ocurre al presionar el botón especificado. Muestra el área del triángulo
+        /// con los datos ingresados
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button2_Click(object sender, EventArgs e)
         {
-            if ((this.textBox1.Text == "") || (this.textBox2.Text == "") || (this.textBox3.Text == "") || (this.textBox4.Text == "") || (this.textBox5.Text == "") || (this.textBox6.Text == ""))
+            double p1coorX, p1coorY, p2coorX, p2coorY, p3coorX, p3coorY;
+            if (!(double.TryParse(this.txtP1CoordX.Text, out p1coorX)) || !(double.TryParse(this.txtP1CoordY.Text, out p1coorY)) || !(double.TryParse(this.txtP2CoordX.Text, out p2coorX)) || !(double.TryParse(this.txtP2CoordY.Text, out p2coorY)) || !(double.TryParse(this.txtP3CoordX.Text, out p3coorX)) || !(double.TryParse(this.txtP3CoordY.Text, out p3coorY)))
             {
-                MessageBox.Show("Faltan datos");
+                MessageBox.Show("Faltan datos o los ingresados no son números", "Verifique los datos");
             }
             else
             {
-                double[] coorX = new double[] { double.Parse(this.textBox1.Text), double.Parse(this.textBox3.Text), double.Parse(this.textBox5.Text) };
-                double[] coorY = new double[] { double.Parse(this.textBox2.Text), double.Parse(this.textBox4.Text), double.Parse(this.textBox6.Text) };
+                double[] coorX = new double[] { p1coorX, p2coorX, p3coorX };
+                double[] coorY = new double[] { p1coorY, p2coorY, p3coorY };
                 double area = this.cFachada.CalcularAreaTriangulo(coorX, coorY);
                 MessageBox.Show("El área del triángulo es: " + area.ToString(), "Área");
             }
